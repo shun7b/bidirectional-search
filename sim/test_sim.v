a19fa76b7edc4935dd36b98b655202a3ee0adfda
shun7b <63786359+shun7b@users.noreply.github.com>
2024-04-21 18:06:44 +0900

    Add files via upload

---
+++ sim/test_sim.v
@@ -0,0 +1,42 @@
+
+`timescale	1ns / 1ns
+`default_nettype none
+
+
+/*Produced by NSL Core(version=20221225), IP ARCH, Inc. Sun Apr 21 09:44:24 2024
+ Licensed to :EVALUATION USER*/
+
+//synthesis translate_off
+/*
+ DO NOT USE ANY PART OF THIS FILE FOR COMMERCIAL PRODUCTS. 
+*/
+module tb;
+	parameter tCYC=2;
+	parameter tPD=(tCYC/10);
+
+	reg p_reset;
+	reg m_clock;
+
+	test test_instance(
+		.p_reset(p_reset),
+		.m_clock(m_clock)
+	);
+
+	initial forever #(tCYC/2) m_clock = ~m_clock;
+
+	initial begin
+		$dumpfile("test.vcd");
+		$dumpvars(0,test_instance);
+	end
+
+	initial begin
+		#(tPD)
+			p_reset = 1;
+			m_clock = 0;
+		#(tCYC)
+			p_reset = 0;
+	end
+
+endmodule
+
+//synthesis translate_on
